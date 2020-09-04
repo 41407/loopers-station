a7e8062bd87e56907e0a69aa82685bee53b35d64
Jimi W <jimi.welling@kuuasema.com>
2020-09-04 15:47:00 +0300

    Add Unity project

---
+++ Assets/FixyController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+public class FixyController : MonoBehaviour
+{
+    [SerializeField] private Vector2 sensitivity = Vector2.one;
+
+    private float steering;
+    private float pedaling;
+    private float speed;
+    [SerializeField, Range(0f, 1f)] private float timeScale = 0.3f;
+    [SerializeField, Range(1f, 5f)] private float steeringAngleCoefficient = 1f;
+    [SerializeField] private Transform fork;
+
+    private float MaximumSteering => Time.deltaTime * sensitivity.x;
+
+    private void FixedUpdate()
+    {
+        Time.timeScale = timeScale;
+        HandleInput();
+        speed += pedaling;
+        if (speed > 20) speed = 20;
+        Steer();
+        Turn();
+
+        Push();
+    }
+
+    private void Steer()
+    {
+        transform.Rotate(0, 0, -steering * Mathf.Clamp01(speed), Space.Self);
+    }
+
+    private void Push()
+    {
+        transform.Translate(0, 0, speed * Time.fixedDeltaTime);
+    }
+
+    private void Turn()
+    {
+        var angularSpeed = -Vector3.SignedAngle(Vector3.up, transform.up, transform.forward) * steeringAngleCoefficient * Mathf.Clamp01(speed) * Time.fixedDeltaTime;
+
+        transform.Rotate(0, Mathf.Sign(angularSpeed) * Mathf.Abs(Mathf.Pow(angularSpeed, 2f)), 0, Space.Self);
+        transform.Rotate(0, 0, Mathf.Clamp(angularSpeed, -MaximumSteering / 2f, MaximumSteering / 2f), Space.Self);
+        fork.localRotation = Quaternion.Euler(fork.localRotation.eulerAngles.x, angularSpeed * 10f, fork.localRotation.eulerAngles.z);
+    }
+
+    void HandleInput()
+    {
+        steering = Input.GetAxis("Horizontal") * Time.deltaTime * sensitivity.x;
+        pedaling = Input.GetAxis("Vertical") * Time.deltaTime * sensitivity.y;
+    }
+}
---
+++ Assets/Follow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(target);
+        transform.position = Vector3.Lerp(transform.position, target.position + target.rotation * offset, 0.05f);
+    }
+}
