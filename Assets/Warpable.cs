5a4cc11922078a357d33b19700a3344aa9bf28d8
Jimi W <jimi.welling@cs.helsinki.fi>
2020-09-18 17:00:53 +0300

    "Refactor" warping

--- Assets/Warpable.cs
+++ Assets/Warpable.cs
@@ -1,35 +1,29 @@
 using UnityEngine;
 
-public class Warpable : MonoBehaviour, IWarpable
-{
-    public void WarpAround(IArea area)
-    {
-        Debug.Log("menäss");
-        if (transform.position.x >= area.Position.x + area.Extents.x)
-        {
-            Debug.Log("OIkeelta ules :)");
-            transform.Translate(new Vector2(area.Extents.x * -2, 0), Space.World);
-        }
-        else if (transform.position.x <= area.Position.x - area.Extents.x)
-        {
-            Debug.Log("vas ules :)");
-            transform.Translate(new Vector2(area.Extents.x * 2, 0), Space.World);
-        }
-
-        if (transform.position.y <= area.Position.y - area.Extents.y)
-        {
-            Debug.Log("alhaalt ules :)");
-            transform.Translate(new Vector2(0, area.Extents.y * 2), Space.World);
-        }
-        else if (transform.position.y >= area.Position.y + area.Extents.y)
-        {
-            Debug.Log("ylhäält ules :)");
-            transform.Translate(new Vector2(0, area.Extents.y * -2), Space.World);
-        }
-    }
-}
 
 internal interface IWarpable
 {
     void WarpAround(IArea area);
+}
+
+public class Warpable : MonoBehaviour, IWarpable
+{
+    public void WarpAround(IArea area)
+    {
+        if (LeavingFromRight(area)) WarpTowards(new Vector2(area.Extents.x * -2, 0));
+        else if (LeavingFromLeft(area)) WarpTowards(new Vector2(area.Extents.x * 2, 0));
+
+        if (LeavingFromBottom(area)) WarpTowards(new Vector2(0, area.Extents.y * 2));
+        else if (LeavingFromTop(area)) WarpTowards(new Vector2(0, area.Extents.y * -2));
+    }
+
+    private bool LeavingFromTop(IArea area) => transform.position.y >= area.Position.y + area.Extents.y;
+    private bool LeavingFromBottom(IArea area) => transform.position.y <= area.Position.y - area.Extents.y;
+    private bool LeavingFromLeft(IArea area) => transform.position.x <= area.Position.x - area.Extents.x;
+    private bool LeavingFromRight(IArea area) => transform.position.x >= area.Position.x + area.Extents.x;
+
+    private void WarpTowards(Vector2 edge)
+    {
+        transform.Translate(edge, Space.World);
+    }
 }
